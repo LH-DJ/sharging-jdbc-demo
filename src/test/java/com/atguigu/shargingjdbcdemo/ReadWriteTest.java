f8b655c6479e8c032f0ea4a69d5761b0be78666f
lh <3189254331@qq.com>
2025-07-04 16:34:54 +0800

    Create ReadWriteTest.java
    添加测试文件

---
+++ src/test/java/com/atguigu/shargingjdbcdemo/ReadWriteTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.shargingjdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReadWriteTest {
+
+    @Test
+    public void test() {
+     System.out.println("读写分离测试");
+    }
+}
